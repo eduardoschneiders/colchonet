227a1c2bdac924e45e0128add05449663d233065
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-01-25 00:07:36 -0200

    Calling confirm_email after save user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      Signup.confirm_email(@user).deliver
       redirect_to @user,
 	          notice: 'User created'
     else
