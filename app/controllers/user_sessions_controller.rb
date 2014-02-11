2e0a144df85729cafa672457fd4bbec34460549b
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-02-11 15:57:33 -0200

    Session control and behavior; Subnav menu creation according if user_signed_in?

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -14,7 +14,8 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
-
+    user_session.destroy
+    redirect_to root_path, notice: t('flash.notice.signed_out')
   end
 
 end
