6b1ebbe14ae725bbcb2ad0b21f6c6596be6804f3
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-02-11 18:12:42 -0200

    Creating relation between user and rooms

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
+  has_many :rooms
+
   validates_presence_of :email, :full_name, :location
   validates_length_of :bio, minimum: 30, allow_blank: false
   #validates_confirmation_of :password
