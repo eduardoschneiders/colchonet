fb632871b20090e96acbd2a7bb0f33863e38c22b
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-12 17:55:32 -0200

    created Review model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211192445) do
+ActiveRecord::Schema.define(version: 20140212195131) do
+
+  create_table "reviews", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "room_id"
+    t.integer  "points"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reviews", ["user_id", "room_id"], name: "index_reviews_on_user_id_and_room_id", unique: true
 
   create_table "rooms", force: true do |t|
     t.string   "title"
