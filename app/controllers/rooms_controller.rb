664a0e3e0d9eaa78a7fb2f55dc68f1bcafcd2494
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-12 17:31:50 -0200

    Implemented most_recent scope on Room model

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,7 @@ class RoomsController < ApplicationController
   # GET /rooms
   # GET /rooms.json
   def index
-    @rooms = Room.all
+    @rooms = Room.most_recent
   end
 
   # GET /rooms/1
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ActiveRecord::Base
+  scope :most_recent, -> {order('created_at DESC')}
   belongs_to :user
 
   def complete_name
