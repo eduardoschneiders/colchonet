e482eeaa8463cc196347b8beb40d1b412a1b4afb
Eduardo Schneiders <eduardo.schneiders@cwi.com.br>
2014-02-12 11:09:34 -0200

    Refatored rooms controller; created current_user_room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,5 @@
 class RoomsController < ApplicationController
   before_action :require_authentication, only: [:new, :edit, :create, :update, :destroy]
-  before_action :set_room, only: [:show, :edit, :update, :destroy]
 
   # GET /rooms
   # GET /rooms.json
@@ -11,15 +10,17 @@ class RoomsController < ApplicationController
   # GET /rooms/1
   # GET /rooms/1.json
   def show
+    @room = Room.find(params[:id])
   end
 
   # GET /rooms/new
   def new
-    @room = Room.new
+    @room = current_user.rooms.build
   end
 
   # GET /rooms/1/edit
   def edit
+    @room = current_user_room
   end
 
   # POST /rooms
@@ -35,7 +36,8 @@ class RoomsController < ApplicationController
   end
 
   def update
-    @room = current_user.rooms.find(params[:id])
+    @room = current_user_room
+
     if @room.update(room_params)
       redirect_to @room, notice: t('flash.notice.room_updated')
     else
@@ -44,14 +46,15 @@ class RoomsController < ApplicationController
   end
 
   def destroy
+    @room = current_user_room
     @room.destroy
     redirect_to rooms_url
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_room
-      @room = Room.find(params[:id])
+    def current_user_room
+      current_user.rooms.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
