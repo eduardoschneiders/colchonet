953b86ad5fccd404fbc75790adcd59f3a9441394
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-13 17:53:55 -0200

    Validations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ActiveRecord::Base
+	POINTS = (1.5).to_a
+
   belongs_to :user
   belongs_to :room, counter_cache: true
+
+  validates_uniqueness_of :user_id, scope: :room_id
+  validates_uniqueness_of :room_id, scope: :user_id
+
+	validates_inclusion_of :points, in: POINTS
 end
