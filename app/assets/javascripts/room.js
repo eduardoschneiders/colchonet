10cd3dcd730f2c1aff958c837a0447a33924a903
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-21 00:42:39 -0300

    Add room.js to control behavior when creating review

---
+++ app/assets/javascripts/room.js
@@ -0,0 +1,24 @@
+$(function(){
+  var $review = $('.review');
+
+  $review.on('ajax:beforeSend', function(){
+    $(this).find('input').attr('disabled', true);
+    console.log('teste');
+  });
+
+  $review.on('ajax:error', function(){
+    replaceButton(this, 'icon-remove', '#B94A48');
+  });
+
+  $review.on('ajax:success', function(){
+    console.log('lsjdf');
+    replaceButton(this, 'icon-ok', '#468847');
+  });
+
+  function replaceButton(container, icon_class, color){
+    $(container).find('input:submit').
+      replaceWith($('<i/>').
+                    addClass(icon_class).
+                    css('color', color));
+  };
+});
