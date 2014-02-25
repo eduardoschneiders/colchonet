7bfd88b351a17c003f93f7a1f9c9adc04810bee8
Eduardo Schneiders <eduardo.m.schneiders@gmail.com>
2014-02-25 18:36:44 -0300

    stars initial behavior

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -20,4 +20,26 @@ $(function(){
                     addClass(icon_class).
                     css('color', color));
   };
+
+  function highlightStars(elem){
+    elem.parent().children('label').removeClass('toggled');
+    elem.addClass('toggled').prevAll('label').addClass('toggld');
+  }
+
+  highlightStars($('.review input:checked + label'));
+
+  var $stars = $('.review input:enabled ~ label');
+
+  $stars.on('mouseenter', function(){
+    highlightStars($(this));
+  });
+
+  $stars.on('mouseleave', function(){
+    highlightStars($('.review input:checked + label'));
+  });
+
+  $('.review input').on('change', function(){
+    stars.off('mouseenter').off('mouseleave').off('click');
+    $(this).parent('form').submit();
+  });
 });
